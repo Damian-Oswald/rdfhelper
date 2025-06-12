f845a063caef87fe4a359f249b1a525f91674082
Damian Oswald <72310532+Damian-Oswald@users.noreply.github.com>
2025-06-12 10:46:05 +0200

    Update helpers.R
    - Handle case where checked value is `character(0)`

--- R/helpers.R
+++ R/helpers.R
@@ -1,18 +1,19 @@
 is.missing <- function(x)
 {
-    if(
-        base::is.null(x) ||
-        base::is.na(x) ||
-        base::is.nan(x) ||
-        x==""
-    )
-    {
-        return(TRUE)
-    }
-    else
-    {
-        return(FALSE)
-    }
+  if(
+    base::length(x) == 0 ||
+    base::is.null(x) ||
+    base::is.na(x) ||
+    base::is.nan(x) ||
+    x==""
+  )
+  {
+    return(TRUE)
+  }
+  else
+  {
+    return(FALSE)
+  }
 }
 rm.quotes <- function(x)
 {
