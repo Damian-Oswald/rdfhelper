f5ff1dcba1dac7fe773bb19f285c2a8525a6c782
Damian Oswald <72310532+Damian-Oswald@users.noreply.github.com>
2025-07-14 15:40:37 +0200

    Update langstring.R
    Allow for multi-line strings

--- R/langstring.R
+++ R/langstring.R
@@ -4,6 +4,7 @@
 #'
 #' @param x Character string. The textual content of the literal.
 #' @param lang Character string. The language tag conforming to [BCP47](https://tools.ietf.org/html/bcp47). Must be provided.
+#' @param multiline Logical. Should the language-tagged string be enclosed with triple-double-qoutes (e.g. `"""Hello, world"""@en`)? `TRUE` allows line breaks within `x`.
 #'
 #' @return A formatted RDF language-tagged literal (`rdf:langString`).
 #'
@@ -12,17 +13,24 @@
 #' langstring("Grüezi", "de-CH")
 #'
 #' @export
-langstring <- function(x, lang = NULL) {
-    if (base::is.null(lang))
-    {
-        stop("A language tag ('lang') must be provided.")
-    }
-    if (base::is.null(x) || base::is.na(x) || x=="")
-    {
-        return(NULL)
-    }
-    x |>
-        base::as.character() |>
-        base::gsub("\"", "'", x = _) |>
-        base::sprintf('"%s"@%s', ... = _, lang)
+langstring <- function(x, lang = NULL, multiline = FALSE) {
+  if (base::is.null(lang))
+  {
+    stop("A language tag ('lang') must be provided.")
+  }
+  if (base::is.null(x) || base::is.na(x) || x=="")
+  {
+    return(NULL)
+  }
+  s <- x |>
+    base::as.character() |>
+    base::gsub("\"", "'", x = _)
+  if (multiline)
+  {
+    base::sprintf('"""\n%s\n"""@%s', s, lang)
+  }
+  else
+  {
+    base::sprintf('"%s"@%s', s, lang)
+  }
 }
